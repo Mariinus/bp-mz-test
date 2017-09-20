f2cea1fb7955518f630c90d69379569115c59bda
Marinus Schepen <mschepen@gmail.com>
2017-09-20 19:10:48 +0200

    get imageload back

--- js/index.js
+++ js/index.js
@@ -19,7 +19,58 @@ $(function(){
     // Set the SVG transform origin.
     DOM.shape.style.transformOrigin = '50% 0%';
 
+    const init = () => {
+        imagesLoaded(document.body, {
+            background: true
+        }, () => document.body.classList.remove('loading'));
+        DOM.enter.addEventListener('click', navigate);
+        DOM.enter.addEventListener('touchenter', navigate);
+        DOM.enter.addEventListener('mouseenter', enterHoverInFn);
+        DOM.enter.addEventListener('mouseleave', enterHoverOutFn);
+    };
 
+    let loaded;
+    const navigate = () => {
+        if (loaded) return;
+        loaded = true;
+
+        anime({
+            targets: DOM.intro,
+            duration: 1100,
+            easing: 'easeInOutSine',
+            translateY: '-200vh'
+        });
+
+        anime({
+            targets: DOM.shape,
+            scaleY: [
+                {
+                    value: [0.8, 1.8],
+                    duration: 550,
+                    easing: 'easeInQuad'
+				}
+				, {
+                    value: 1,
+                    duration: 550,
+                    easing: 'easeOutQuad'
+				}
+			]
+        });
+
+        anime({
+            targets: DOM.path,
+            duration: 1100,
+            easing: 'easeOutQuad',
+            d: DOM.path.getAttribute('pathdata:id')
+        });
+    };
+
+    let isActive;
+    let enterTimeout;
+
+
+    init();
+};
 
 // Contact page
 $(function () {
