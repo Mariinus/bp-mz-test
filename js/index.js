5d9bb14bd4ea6b89dc1fcf677dcb9b93766ac966
Marinus Schepen <mschepen@gmail.com>
2017-09-21 19:11:52 +0200

    IOS viewport test

--- js/index.js
+++ js/index.js
@@ -8,6 +8,18 @@ $(function(){
   });
 });
 
+import * as viewportUnitsBuggyfill from 'viewport-units-buggyfill';
+
+// …
+
+// Initialize viewportUnitsBuggyfill
+viewportUnitsBuggyfill.init();
+
+// Also hook viewportUnitsBuggyfill to resize event (if it was initialized)
+if (document.getElementById('patched-viewport')) {
+    window.addEventListener('resize', viewportUnitsBuggyfill.refresh, true);
+}
+
 {
     const DOM = {};
     DOM.intro = document.querySelector('.content--intro');
