44831fc7c236882097f977df3a68ba782c86ce67
Marinus Schepen <mschepen@gmail.com>
2017-09-21 19:27:09 +0200

    test fix

--- js/contactpage.js
+++ js/contactpage.js
@@ -1,15 +1,3 @@
-import * as viewportUnitsBuggyfill from 'viewport-units-buggyfill';
-
-// …
-
-// Initialize viewportUnitsBuggyfill
-viewportUnitsBuggyfill.init();
-
-// Also hook viewportUnitsBuggyfill to resize event (if it was initialized)
-if (document.getElementById('patched-viewport')) {
-    window.addEventListener('resize', viewportUnitsBuggyfill.refresh, true);
-}
-
 // Contact page
 $(function() {
   var $overlay = $('.overlay'),
